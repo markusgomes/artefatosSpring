0241055080eff1fb2e417ba986efc250580e5a7d
markusgomes <contato.markus@gmail.com>
2024-02-27 19:54:43 -0300

    first commit

---
+++ src/main/java/labiv/markusgomes/ativ1/Ativ1Application.java
@@ -0,0 +1,13 @@
+package labiv.markusgomes.ativ1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ativ1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ativ1Application.class, args);
+	}
+
+}
---
+++ src/test/java/labiv/markusgomes/ativ1/Ativ1ApplicationTests.java
@@ -0,0 +1,13 @@
+package labiv.markusgomes.ativ1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ativ1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
